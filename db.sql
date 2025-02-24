f6ba09108d254c70e300cda1b99e2be9dfe679f5
KevinNot\Kevin <lkevinvinagre@hotmail.com>
2025-02-24 16:59:29 -0400

    database planning

---
+++ db.sql
@@ -0,0 +1,15 @@
+-- Criar banco de dados
+
+-- Criar Tabela de usuario
+
+-- Criar Tabela de Visitantes
+
+-- Criar Tabela de Administradores
+
+-- Criar Tabela de Instituto
+
+-- Criar Tabela de Evento
+
+-- Criar Tabela de Projetos
+
+-- Criar Tabela de Votos
