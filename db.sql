898158cde93fdd92f4627e1adb5f61c5fca2b94a
lkevinvinagre <lkevinvinagre@hotmail.com>
2025-02-24 20:06:55 -0400

    database tables creation

--- db.sql
+++ db.sql
@@ -1,15 +1,74 @@
 -- Criar banco de dados
 
+    CREATE DATABASE vottech IF NOT EXISTS;
+    USE vottech;
+
 -- Criar Tabela de usuario
+
+    CREATE TABLE user IF NOT EXISTS(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(150) NOT NULL,
+        pass TEXT NOT NULL,
+        email VARCHAR(150) NOT NULL,
+        status CHAR(1) DEFAULT 'A'
+    );
 
 -- Criar Tabela de Visitantes
 
--- Criar Tabela de Administradores
+    CREATE TABLE visitor IF NOT EXISTS(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        code VARCHAR(150),
+        faceData TEXT NOT NULL,
+        userId INT NOT NULL,
+        FOREIGN KEY (userId) REFERENCES user(id)
+    );
 
 -- Criar Tabela de Instituto
 
+    CREATE TABLE institute IF NOT EXISTS(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        CNPJ VARCHAR(21),
+        iname VARCHAR(255) NOT NULL
+    );
+
+-- Criar Tabela de Administradores
+
+
+    CREATE TABLE admin IF NOT EXISTS(
+        uId INT PRIMARY KEY NOT NULL,
+        iId INT PRIMARY KEY NOT NULL,
+        role VARCHAR(255) NOT NULL,
+        FOREIGN KEY (uId) REFERENCES user(id),
+        FOREIGN KEY (iId) REFERENCES institute(Id)
+    );
+
 -- Criar Tabela de Evento
+
+    CREATE TABLE event IF NOT EXISTS(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description TEXT NOT NULL,
+        initDate DATE NOT NULL,
+        endDate DATE NOT NULL,
+
+    );
 
 -- Criar Tabela de Projetos
 
+    CREATE TABLE project IF NOT EXISTS(
+        ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description TEXT NOT NULL,
+        eId INT NOT NULL,
+        FOREIGN KEY (eId) REFERENCES event(id)
+    );
+
 -- Criar Tabela de Votos
+
+    CREATE TABLE vote IF NOT EXISTS(
+        vId INT NOT NULL PRIMARY KEY,
+        pId INT NOT NULL PRIMARY KEY,
+        voteDate DATE NOT NULL,
+        FOREIGN KEY (vId) REFERENCES visitor(id),
+        FOREIGN KEY (pId) REFERENCES project(id)
+    );
