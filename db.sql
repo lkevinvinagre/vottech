d41148583f43036a173e039eb2e748508ea8ae0b
KevinNot\Kevin <lkevinvinagre@hotmail.com>
2025-02-25 09:06:38 -0400

    sql adjust

--- db.sql
+++ db.sql
@@ -22,10 +22,50 @@
 
 -- Criar Tabela de Instituto
 
+    CREATE TABLE IF NOT EXISTS institute(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        CNPJ VARCHAR(21),
+        iname VARCHAR(255) NOT NULL
+    );
+
 -- Criar Tabela de Administradores
+
+    CREATE TABLE IF NOT EXISTS admin(
+        uId INT PRIMARY KEY NOT NULL,
+        iId INT PRIMARY KEY NOT NULL,
+        role VARCHAR(255) NOT NULL,
+        FOREIGN KEY (uId) REFERENCES user(id),
+        FOREIGN KEY (iId) REFERENCES institute(Id)
+    );
 
 -- Criar Tabela de Evento
 
+    CREATE TABLE IF NOT EXISTS event(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description TEXT NOT NULL,
+        initDate DATE NOT NULL,
+        endDate DATE NOT NULL,
+        iId INT NOT NULL,
+        FOREIGN KEY (iId) REFERENCES institute(id)
+    );
+
 -- Criar Tabela de Projetos
 
+    CREATE TABLE IF NOT EXISTS project(
+        ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description TEXT NOT NULL,
+        eId INT NOT NULL,
+        FOREIGN KEY (eId) REFERENCES event(id)
+    );    
+
 -- Criar Tabela de Votos
+
+    CREATE TABLE IF NOT EXISTS vote(
+        vId INT NOT NULL PRIMARY KEY,
+        pId INT NOT NULL PRIMARY KEY,
+        voteDate DATE NOT NULL STANDARD CURRENT_TIMESTAMP,
+        FOREIGN KEY (vId) REFERENCES visitor(id),
+        FOREIGN KEY (pId) REFERENCES project(id)
+    );
