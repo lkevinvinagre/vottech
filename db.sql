e9ec95eecfa88dea6fca98749a410c9ff0c33c5f
KevinNot\Kevin <lkevinvinagre@hotmail.com>
2025-02-25 08:40:07 -0400

    visitor table task complete

--- db.sql
+++ db.sql
@@ -4,6 +4,14 @@
 
 -- Criar Tabela de Visitantes
 
+    CREATE TABLE IF NOT EXISTS visitor(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        code VARCHAR(150),
+        faceData TEXT NOT NULL,
+        userId INT NOT NULL,
+        FOREIGN KEY (userId) REFERENCES user(id)
+    );
+
 -- Criar Tabela de Instituto
 
 -- Criar Tabela de Administradores
