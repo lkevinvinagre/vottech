0e6d85e436ec05f6131b3edb49dac1c1aaf03997
KevinNot\Kevin <lkevinvinagre@hotmail.com>
2025-02-26 09:08:59 -0400

    population adjust

--- population.sql
+++ population.sql
@@ -1,5 +1,5 @@
 --População da tabela de usuarios
-INSERT INTO user (username, pass, email, status) VALUES ('Alberto',MD5('123'),'email@email.com','A');
-INSERT INTO user (username, pass, email, status) VALUES ('Bianca',MD5('231'),'email@email.com','A');
-INSERT INTO user (username, pass, email, status) VALUES ('Carlos',MD5('173'),'email@email.com','A');
-INSERT INTO user (username, pass, email, status) VALUES ('Eduardo',MD5('183'),'email@email.com','A');
+INSERT INTO user (username, pass, email) VALUES ('Alberto',MD5('123'),'email@email.com');
+INSERT INTO user (username, pass, email) VALUES ('Bianca',MD5('231'),'email@email.com');
+INSERT INTO user (username, pass, email) VALUES ('Carlos',MD5('173'),'email@email.com');
+INSERT INTO user (username, pass, email) VALUES ('Eduardo',MD5('183'),'email@email.com');
